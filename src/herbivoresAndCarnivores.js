3792293736dc4a45fdd2f05396d75d1267977f41
Oleg <scor1@ukr.net>
2024-12-07 13:27:03 +0100

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  static deadAnimals() {
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  // eslint-disable-next-line constructor-super
+  constructor(name, health, hidden) {
+    // eslint-disable-next-line no-undef
+    super(name, health);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  // eslint-disable-next-line no-useless-constructor
+  constructor(name, health, hidden) {
+    super(name, health, hidden);
+  }
+
+  bite(item) {
+    // eslint-disable-next-line no-constant-condition
+    if (item.hidden === false && item instanceof Herbivore) {
+      // eslint-disable-next-line no-unused-expressions
+      item.health -= 50;
+      Animal.deadAnimals();
+    }
+  }
 }
 
 module.exports = {
